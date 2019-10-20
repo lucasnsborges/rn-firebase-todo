25e571f04775653dfe1f434f26f6d1a264cc887e
Lucas Borges <lucasnsborges@gmail.com>
2019-10-20 11:50:57 -0300

    feat: add app and firebase config

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import './fixtimerbug';
+import React from 'react';
+import { initializeApp } from 'firebase';
+import { StyleSheet, StatusBar, View } from 'react-native';
+
+import Todo from './src';
+import firebaseConfig from './config';
+
+initializeApp(firebaseConfig);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Todo />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0496FF',
+    paddingTop: StatusBar.currentHeight
+  },
+});
+
---
+++ config.ts
@@ -0,0 +1,6 @@
+export default {
+  apiKey: "AIzaSyAJto5a91kcZuAsgZazTpj7MYuxQaKoY28",
+  authDomain: "rn-todo-hupper.firebaseapp.com",
+  databaseURL: "https://rn-todo-hupper.firebaseio.com",
+  storageBucket: "rn-todo-hupper.appspot.com",
+}
